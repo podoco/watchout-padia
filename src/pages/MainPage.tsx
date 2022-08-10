d3528d626e0cfcea513d833bb2fe116d45008398
podoco <103816228+podoco@users.noreply.github.com>
2022-08-10 15:26:33 +0900

    Router

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const MainPage: React.FC = () =>{
+    return(
+        <div>Main Page</div>
+    )
+}
+
+
+export default MainPage;
---
+++ src/pages/MovieDetail.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const MovieDetail: React.FC = () =>{
+    return(
+        <div>MovieDetail</div>
+    )
+}
+
+
+export default MovieDetail;
